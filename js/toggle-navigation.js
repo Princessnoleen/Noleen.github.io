b40e0c0504bbc8810b70ed243f8a6cc47b3f65d2
Noleen D. Melnikov <princessnoleen04@gmail.com>
2016-11-19 17:06:45 -0800

    Update toggle-navigation.js

--- js/toggle-navigation.js
+++ js/toggle-navigation.js
@@ -1,3 +1,6 @@
+<script type="text/javascript"  src="https://code.jquery.com/jquery-1.10.0.min.js"></script>	
+<script type="text/javascript" src="js/toggle-navigation.js"></script>
+
 $(document).ready(function() {
 	$('body').addClass('js');
 	var $menu = $('#menu'),
